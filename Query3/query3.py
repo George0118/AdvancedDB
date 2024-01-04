65299adb0aa7af451b07fed8faebb48632697d45
ntua-el19120 <115192395+ntua-el19120@users.noreply.github.com>
2024-01-04 20:22:51 +0200

    Update query3.py

--- Query3/query3.py
+++ Query3/query3.py
@@ -1,12 +1,12 @@
 # Import necessary libraries
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, year, to_timestamp
 
 # Create a Spark session
 spark = SparkSession.builder.appName("Query 3 DF/SQL").getOrCreate()
 
 # Define columns to select
-columns_to_select1 = ["DR_NO", "Time OCC", "Vict Descent", "LAT", "LON"]
+columns_to_select1 = ["DR_NO", "Date Rptd", "Vict Descent", "LAT", "LON"]
 columns_to_select2 = ["Zip Code", "Estimated Median Income"]
 columns_to_select3 = ["LAT", "LON", "ZIPcode"]
 
@@ -19,9 +19,33 @@ reverse_geocoding_data = spark.read.csv("file:///home/user/data/revgecoding.csv"
 income_data.createOrReplaceTempView("income_data_table")
 
 
+# Convert "Date Rptd" to a timestamp column
+crimes_df1 = crimes_df1.withColumn("Timestamp", to_timestamp("Date Rptd", "MM/dd/yyyy hh:mm:ss a"))
 
-crimes_df1 = crimes_df1.filter(col("Vict Descent").isNotNull())
+# Extract the year from the timestamp
+crimes_df1 = crimes_df1.withColumn("Year", year("Timestamp"))
+
+# Show the DataFrame
+# crimes_df1.show(truncate=False)
+
+
+
+
+
+
+crimes_df1_2015 = crimes_df1.filter(crimes_df1["Year"] == 2015)
+
+crimes_df1_2015 = crimes_df1_2015.filter(col("Vict Descent").isNotNull())
+# count = crimes_df1_2015.count()
+# print("Number of rows in crimes_df1_2015:", count)
+
+
+#2. Στις περιπτώσεις που στο σύνολο δεδομένων Reverse Geocoding αναφέρονται περισσότερα
+#του ενός ZIP Codes για ένα ζεύγος συντεταγμένων, θα πρέπει να χρησιμοποιήσετε ένα από
+#αυτά.
 reverse_geocoding_data = reverse_geocoding_data.dropDuplicates(["LAT", "LON"])
+
+
 # Write SQL queries to get top 3 and bottom 3 rows
 top_income_sql = (
     "SELECT DISTINCT `Zip Code`, CAST(regexp_replace(`Estimated Median Income`, '[$,]', '') AS INT) AS income "
@@ -44,12 +68,17 @@ combined_incomes = top_incomes.union(bottom_incomes).distinct()
 # combined_incomes.show(truncate=False)
 
 # First Join
-first_join = crimes_df1.join(reverse_geocoding_data, ["LAT", "LON"], "inner")
+first_join = crimes_df1_2015.join(reverse_geocoding_data, ["LAT", "LON"], "inner")
+# first_join.show(truncate=False)
+# row_count = first_join.count()
+# print("Number of rows in first_join:", row_count)
+
+
 
 # Second Join
 merged_data = first_join.join(combined_incomes, first_join["ZIPcode"] == combined_incomes["Zip Code"], "inner")
-
-# Show the resulting DataFrame
+count2 = merged_data.count()
+# print("Number of rows in merged_data:", count2)
 # merged_data.show(truncate=False)
 
 # Register the merged_data DataFrame as a temporary SQL table
@@ -81,7 +110,7 @@ victim_descent_counts_sql = """
             ELSE 'Unknown' 
         END AS Descent_Code,
         COUNT(*) as count 
-    FROM merged_data_table 
+    FROM merged_data_table
     GROUP BY Descent_Code
     ORDER BY count DESC
 
@@ -93,9 +122,6 @@ victim_descent_counts = spark.sql(victim_descent_counts_sql)
 # Show the result
 victim_descent_counts.show(truncate=False)
 
-
-
-# Further processing or analysis can be performed on the merged_data DataFrame as needed.
 
 # Stop the Spark session
 spark.stop()
